020b142fcf41080810edb988c0ee8ece0f5e07fb
lucasjsr2 <lucasjuan14@gmail.com>
2020-07-24 00:10:09 -0300

    up site 3

---
+++ site 4/README.md
@@ -0,0 +1,11 @@
+## Instalar dependências
+```javascript
+npm install
+```
+
+## SCSS
+Para compilar o scss e ficar ouvindo mudanças no arquivo:
+```javascript
+npm sass
+```
+
--- site 4/index.js
+++
@@ -1,9 +0,0 @@
-var text = document.querySelectorAll('.faq-text');
-var increase = document.querySelectorAll('.faqs');
-
-document.querySelectorAll('.btn-show').forEach((element, index) => {
-  element.addEventListener('click', event => {
-    increase[index].classList.toggle('increase')
-    text[index].classList.toggle('show')
-  })
-});
